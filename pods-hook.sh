546d846609fa53fb84b22194e90fba892096f669
4220182 <4220182@qq.com>
2020-08-24 12:44:07 +0800

    TEST

--- pods-hook.sh
+++ pods-hook.sh
@@ -20,10 +20,12 @@ else
   echo "TEST Start"
 
   type=$(jq -r '.[0].type' $BINDING_CONTEXT_PATH)
+  bindingName=`jq -r ".[$IND].binding" $BINDING_CONTEXT_PATH`
   resourceEvent=$(jq -r '.[0].watchEvent' $BINDING_CONTEXT_PATH)
   resourceName=$(jq -r '.[0].object.metadata.name' $BINDING_CONTEXT_PATH)
 
   echo "TEST type: '${type}'"
+  echo "TEST bindingName: '${bindingName}'"
   echo "TEST resourceEvent: '${resourceEvent}'"
   echo "TEST resourceName: '${resourceName}'"
   cat $BINDING_CONTEXT_PATH >/tmp/context.json
